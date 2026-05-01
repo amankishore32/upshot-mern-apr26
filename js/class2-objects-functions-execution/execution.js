3cc035697288e1254ba6c4d187fbfa91c67e8205
amankishore32 <aman.kishore32@gmail.com>
2026-05-01 20:01:50 +0530

    Execution context and Functions

---
+++ js/class2-objects-functions-execution/execution.js
@@ -0,0 +1,13 @@
+var a = 3;
+var b = 4;
+
+function add(num1, num2) {
+  var ans = num1 + num2;
+  return ans;
+}
+
+var sum1 = add(2, 3);
+var sum2 = add(a, b);
+
+console.log(sum1);
+console.log(sum2);
---
+++ js/class2-objects-functions-execution/functions.js
@@ -0,0 +1,24 @@
+// Function to say Hello
+function greet() {
+  console.log("Hello, JS");
+}
+
+greet();
+
+// Functions using arguments
+
+function add(a, b) {
+  console.log(a + b);
+}
+
+add(2, 3);
+
+// Returning Function
+
+function sum(a, b) {
+  return a + b;
+}
+
+let result = sum(3, 7);
+
+console.log(result + 10);
