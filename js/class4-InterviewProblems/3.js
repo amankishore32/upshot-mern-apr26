a4eecb5395942d6c5c4cbfd64556508a46d3fdba
amankishore32 <aman.kishore32@gmail.com>
2026-05-08 19:49:58 +0530

    Students grading

---
+++ js/class4-InterviewProblems/3.js
@@ -0,0 +1,25 @@
+const students = [
+  { name: "Aziz", score: 92 },
+  { name: "Amit", score: 80 },
+  { name: "Tushar", score: 60 },
+  { name: "Saadiq", score: 98 },
+  { name: "Aman", score: 10 },
+];
+
+// Grade Chart
+// "A" -> 90-100
+// "B" -> 75-89
+// "C" -> below 75
+
+// Output
+// {
+//    A: ["Aziz", "Saadiq"],
+//    B: ["Amit"],
+//    C: ["Tushar", "Aman"]
+// }
+
+function studentGrading(studentList) {
+  // Write your code here
+}
+
+console.log(studentGrading(student));
