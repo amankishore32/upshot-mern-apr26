1335b47951665c8c71183d8a24108534a61d86c7
amankishore32 <aman.kishore32@gmail.com>
2026-05-08 19:27:50 +0530

    Temprature conversion solution

--- js/class4-InterviewProblems/1.js
+++ js/class4-InterviewProblems/1.js
@@ -1,4 +1,4 @@
-// ConvertTemperature(temp, unit) 
+// ConvertTemperature(temp, unit)
 
 // 1. Convert the Celcius to Fahrenheit and vice-versa
 // 2. Return the converted temprature
@@ -8,16 +8,24 @@
 // Boilerplate code
 
 function CelToFah(celsius) {
-	return celsius * 9 / 5 + 32;
+  return (celsius * 9) / 5 + 32;
 }
 
-function FahToCel(fahrenheit){
-	return (farenheit - 32) * 5 / 9;
+function FahToCel(fahrenheit) {
+  return ((fahrenheit - 32) * 5) / 9;
 }
 
-function convertTemperature(temp, unit){
-	// Write code here
+function convertTemperature(temp, unit) {
+  // Write code here
+  if (unit === "C") {
+    return CelToFah(temp) + "F";
+  } else if (unit === "F") {
+    return FahToCel(temp) + "C";
+  } else {
+    return "Invalid Input, Please enter F or C";
+  }
 }
 
 console.log(convertTemperature(100, "C")); // "212F"
 console.log(convertTemperature(32, "F")); // "0C"
+console.log(convertTemperature(32, "G"));
