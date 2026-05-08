50278f4585f2fcd8ba8a6b99e4be01156b181473
amankishore32 <aman.kishore32@gmail.com>
2026-05-08 19:30:31 +0530

    Reverse words

---
+++ js/class4-InterviewProblems/2.js
@@ -0,0 +1,13 @@
+// reverseWords(sentence)
+// 1. Take a String sentence as the input
+// 2. Return a new string where the order of the words is reversed.
+
+// Eg. "Hello world" -> "world Hello"
+// Hint: Think it as array of strings
+
+function reverseWords(sentence) {
+  // Write code here
+}
+
+console.log(reverseWords("Hello world")); // "world Hello"
+console.log(reverseWords("Javascript is fun")); // "fun is Javascript"
