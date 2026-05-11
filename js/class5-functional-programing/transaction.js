cae7ebaac02fbb58db48a65593aca0788367b709
amankishore32 <aman.kishore32@gmail.com>
2026-05-11 20:26:25 +0530

    Transaction solution

--- js/class5-functional-programing/transaction.js
+++ js/class5-functional-programing/transaction.js
@@ -2,3 +2,12 @@ const transactions = [1000, 3000, 4000, 2000, -898, 3800, -4500];
 const inrToUsd = 95.26;
 
 // use map method and show the amounts in USD
+
+let UDSvalues = transactions.map(function (amount) {
+  return displayUSDValue(amount);
+});
+console.log(UDSvalues);
+
+function displayUSDValue(transactionAmount) {
+  return inrToUsd * transactionAmount;
+}
