58ba1672b44536c8941ad0b52736e7b2e7b2b974
amankishore32 <aman.kishore32@gmail.com>
2026-06-19 19:48:45 +0530

    Classes

--- js/class16-classes/class.js
+++ js/class16-classes/class.js
@@ -1,23 +1,33 @@
-// Toppings ->
-// Size ->
-// Crust Type ->
+let x = 10;
+class Pizza {
+  constructor(toppings1, size1, crust1) {
+    this.crust = crust1;
+    this.size = size1;
+    this.toppings = toppings1;
+  }
 
-function Pizza(toppings1, size1, crust1) {
-  this.toppings = toppings1;
-  this.size = size1;
-  this.crust = crust1;
-
-  this.cook = function () {
+  cook() {
     console.log(
       `Pizza with ${this.toppings} of size ${this.size} and crust ${this.crust} is ready`,
     );
-  };
-
-  // console.log(this);
+  }
 }
 
 let customerOrder1 = new Pizza(["cappsicum", "olives"], "small", "thin");
 let customerOrder2 = new Pizza(["cappsicum", "olives"], "medium", "thin");
 
 console.log(customerOrder1);
+
 console.log(customerOrder2);
+
+class Drink {
+  sip() {
+    console.log("Sipping");
+  }
+}
+
+let drink1 = new Drink();
+let drink2 = new Drink();
+
+console.log(drink1);
+console.log(drink2);
