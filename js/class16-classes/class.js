cf4a549e19ac2419370a82405b9e1b74073d2501
amankishore32 <aman.kishore32@gmail.com>
2026-06-19 20:19:33 +0530

    Inheritance

--- js/class16-classes/class.js
+++ js/class16-classes/class.js
@@ -1,33 +1,54 @@
 let x = 10;
 class Pizza {
+  static totalPizza = 0;
+
   constructor(toppings1, size1, crust1) {
     this.crust = crust1;
     this.size = size1;
     this.toppings = toppings1;
+    Pizza.totalPizza++;
+  }
+
+  static getPizzaCount() {
+    console.log(`Total pizzas baked = ${Pizza.totalPizza}`);
   }
 
   cook() {
-    console.log(
-      `Pizza with ${this.toppings} of size ${this.size} and crust ${this.crust} is ready`,
-    );
+    return `Pizza with ${this.toppings} of size ${this.size} and crust ${this.crust} is ready`;
   }
 }
 
 let customerOrder1 = new Pizza(["cappsicum", "olives"], "small", "thin");
 let customerOrder2 = new Pizza(["cappsicum", "olives"], "medium", "thin");
 
-console.log(customerOrder1);
+// console.log(customerOrder1);
 
-console.log(customerOrder2);
+// console.log(customerOrder2);
 
-class Drink {
+class Meal extends Pizza {
+  static totalMeals = 0;
+  constructor(toppings, pizzaSize, pizzaCrust, drink) {
+    super(toppings, pizzaSize, pizzaCrust); // Access the Pizza(Parent class)
+    this.drink = drink;
+    Meal.totalMeals++;
+  }
+
   sip() {
-    console.log("Sipping");
+    return `${this.drink}`;
+  }
+
+  static getMealCount() {
+    console.log(`Total Meals made = ${Meal.totalMeals}`);
+  }
+
+  deliverMeal() {
+    let readyDrink = this.sip();
+    let readyPizza = super.cook();
+    console.log(`Meal with ${readyPizza} and ${readyDrink} is served.`);
   }
 }
 
-let drink1 = new Drink();
-let drink2 = new Drink();
+let meal1 = new Meal(["onions, tomato"], "medium", "thick", "coke");
 
-console.log(drink1);
-console.log(drink2);
+Pizza.getPizzaCount();
+Meal.getMealCount();
