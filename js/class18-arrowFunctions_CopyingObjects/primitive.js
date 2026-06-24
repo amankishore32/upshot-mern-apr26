5e4ea9244479708e1e3d82ad847315c60137ae99
amankishore32 <aman.kishore32@gmail.com>
2026-06-24 20:48:48 +0530

    Primitive and Reference DT

---
+++ js/class18-arrowFunctions_CopyingObjects/primitive.js
@@ -0,0 +1,5 @@
+let numOne = 50;
+let numTwo = numOne;
+numOne = 100;
+console.log(numOne);
+console.log(numTwo);
---
+++ js/class18-arrowFunctions_CopyingObjects/refrenceDT.js
@@ -0,0 +1,10 @@
+let obj1 = {
+  name: "Saadique",
+  age: 26,
+};
+
+let obj2 = obj1;
+
+obj1.age = 27;
+console.log(obj1);
+console.log(obj2);
